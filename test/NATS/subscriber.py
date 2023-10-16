864b8b8189b7a1b93858f0c398d718b8d52988f5
Shiwon Park <psw041115@gmail.com>
2023-10-17 02:57:44 +0900

    sub

--- test/NATS/subscriber.py
+++ test/NATS/subscriber.py
@@ -8,17 +8,18 @@ BUCKET_NAME = "stream"
 DEST_FOLDER = "D:\\competition\\net challenge\\Network-infra-kafka\\test\\image"
 
 async def main():
+    print("NATS subscriber")
     nc = nats.NATS()
-    
     await nc.connect("nats://10.32.187.188:4222")
     minio = Minio('10.32.187.188:9000', access_key=ACCESS_KEY, secret_key=SECRET_KEY, secure=False)
-    minio.fget_object(BUCKET_NAME, "index-0_timestamp-2023-10-15-03-22-08.jpg", DEST_FOLDER + "/" + "index-0_timestamp-2023-10-15-03-22-08.jpg")
+    print("Connected to NATS server, and Minio server")
     
     async def message_handler(msg):
         subject = msg.subject
         reply = msg.reply
         data = msg.data.decode()
         print(f"Received a message on '{subject} {reply}': {data}")
+        minio.fget_object(BUCKET_NAME, data, DEST_FOLDER + "/" + data)
         
         
     # Simple publisher and async subscriber via coroutine.
